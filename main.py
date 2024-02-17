49369acab067f54f7666b709b9b13539f8728a56
wcolglazier <139183369+wcolglazier@users.noreply.github.com>
2024-02-17 15:44:13 -0600

    Very low level

--- main.py
+++ main.py
@@ -15,16 +15,14 @@ while True:
     print(f'Bar 2 is {bar2}/5 fun rn')
     print(f'Bar 3 is {bar3}/5 fun rn')
 
-    Which_bar = int(input('Which bar do you wanna rate from 1-3: '))
+    Which_bar, bar_rate = map(int, input('Enter bar number (1-3) and rating (1-5), separated by a comma: ').split(','))
+
     if Which_bar == 1:
-        bar_rate_1 = int(input('Enter rating for bar 1 from 1 to 5: '))
-        total_rating1 += bar_rate_1
+        total_rating1 += bar_rate
         count1 += 1
     elif Which_bar == 2:
-        bar_rate_2 = int(input('Enter rating for bar 2 from 1 to 5: '))
-        total_rating2 += bar_rate_2
+        total_rating2 += bar_rate
         count2 += 1
     elif Which_bar == 3:
-        bar_rate_3 = int(input('Enter rating for bar 3 from 1 to 5: '))
-        total_rating3 += bar_rate_3
+        total_rating3 += bar_rate
         count3 += 1
